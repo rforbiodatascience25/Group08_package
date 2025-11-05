7983755836be2b61ed3ca27f7cd35ba8b5f7c8be
daniela-d-d <daniela.d.d@outlook.com>
2025-11-05 14:55:56 +0100

    created codon_table and description for it

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Standard codon table for centraldog
+#'
+#' This codon table can be used to replicate the process of changing DNA to RNA and vise versa aka central dogma of molecular biology
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
